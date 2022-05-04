2dd46435d5980173861c4dadd5793990f9016d20
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-05-04 13:07:33 +0100

    input screen styles

--- src/components/LogInput/LogInput.jsx
+++ src/components/LogInput/LogInput.jsx
@@ -11,44 +11,53 @@ const LogInput = ({
   return (
     <div className="logInput-container">
       <div className="logInput">
-        <label htmlFor="title">title</label>
+        <label htmlFor="title">Title</label>
         <input
+          className="logInput__input"
           type="text"
           name="title"
           onChange={handleInput}
           value={state.title}
         />
-        <label htmlFor="problemImage">ProblemImage</label>
+        <label htmlFor="problemImage">Screenshot of the problem</label>
         <input
+          className="logInput__input"
           type="text"
           // accept="image/png, image/jpeg"
           name="problemImage"
           onChange={handleInput}
           value={state.problemImage}
         />
-        <label htmlFor="summary">summary</label>
+        <label htmlFor="summary">Summary</label>
         <input
+          className="logInput__summary"
           type="text"
           name="summary"
           onChange={handleInput}
           value={state.summary}
         />
-        <label htmlFor="solutionImage">solutionImage</label>
+        <label htmlFor="solutionImage">Screenshot of the solution</label>
         <input
+          className="logInput__input"
           type="text"
           name="solutionImage"
           onChange={handleInput}
           value={state.solutionImage}
         />
-        <label htmlFor="solutionSummary">solutionSummary</label>
+        <label htmlFor="solutionSummary">SolutionSummary</label>
         <input
+          className="logInput__summary"
           type="text"
           name="solutionSummary"
           onChange={handleInput}
           value={state.solutionSummary}
         />
-        <button onClick={saveButton}>SAVE</button>
-        <button onClick={handleAddSectionState}>BACK</button>
+        <button className="logInput__button" onClick={saveButton}>
+          SAVE
+        </button>
+        <button className="logInput__button" onClick={handleAddSectionState}>
+          BACK
+        </button>
       </div>
     </div>
   );
