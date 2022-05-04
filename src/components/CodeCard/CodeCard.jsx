d7fc5f2b0bbaafb81aefb69b897689b5f8fb5745
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-05-04 12:23:33 +0100

    created css pin for cards

--- src/components/CodeCard/CodeCard.jsx
+++ src/components/CodeCard/CodeCard.jsx
@@ -10,12 +10,12 @@ const CodeCard = ({
 }) => {
   return (
     <div className="codeCard">
-      <h1>{id}</h1>
-      <h2>{title}</h2>
+      <div className="pin"></div>
+      <h3 className="codeCard__id">{id}</h3>
+      <h2 className="codeCard__heading">{title}</h2>
       {/* <img src={problemImage} alt="" /> */}
       <h3> {problemImage} </h3>
-
-      <p>{summary}</p>
+      <p className="codeCard__summary">{summary}</p>
       <h3>{solutionPic} </h3>
       {/* <img
         src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
@@ -23,7 +23,7 @@ AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
     9TXL0Y4OHwAAAABJRU5ErkJggg=="
         alt=""
       /> */}
-      <p>{solutionSummary}</p>
+      <p className="codeCard__summary">{solutionSummary}</p>
     </div>
   );
 };
